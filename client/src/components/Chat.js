2e29633017821b3e50a0c1d27ea77df77a6c4e16
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-11-23 02:33:52 +0800

    first commit

---
+++ client/src/components/Chat.js
@@ -0,0 +1,26 @@
+const Chat = ({descendingOrderMessages}) => {
+    return (
+        <>
+            <div className="chat-display">
+                {descendingOrderMessages.map((message, _index) => (
+                    <div key={_index}>
+                        <div className="chat-message-header">
+                            <div className="img-container"> 
+                            
+                                <img src={message.img} alt={message.name + ' profile'}/>
+                                
+                            </div>
+                                        <div className="message-username">
+                                            <p>{message.name}</p>
+                                        </div>
+                        </div>
+                        
+                        <p>{message.message}</p>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default Chat
