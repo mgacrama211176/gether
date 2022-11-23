4aad837743c681ebf8002faf4d643e538b57195c
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-11-23 11:40:40 +0800

    fixed minor bugs and error trappings added styled components to easy manuever media

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,26 +1,24 @@
-const Chat = ({descendingOrderMessages}) => {
-    return (
-        <>
-            <div className="chat-display">
-                {descendingOrderMessages.map((message, _index) => (
-                    <div key={_index}>
-                        <div className="chat-message-header">
-                            <div className="img-container"> 
-                            
-                                <img src={message.img} alt={message.name + ' profile'}/>
-                                
-                            </div>
-                                        <div className="message-username">
-                                            <p>{message.name}</p>
-                                        </div>
-                        </div>
-                        
-                        <p>{message.message}</p>
-                    </div>
-                ))}
+const Chat = ({ descendingOrderMessages }) => {
+  return (
+    <>
+      <div className="chat-display">
+        {descendingOrderMessages.map((message, _index) => (
+          <div key={_index}>
+            <div className="chat-message-header">
+              <div className="img-container">
+                <img src={message.img} alt={message.name + " profile"} />
+              </div>
+              <div className="message-username">
+                <p>{message.name}</p>
+              </div>
             </div>
-        </>
-    )
-}
 
-export default Chat
+            <p>{message.message}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Chat;
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,31 +1,74 @@
+import React from "react";
 import whiteLogo from "../images/gether-logo.png";
 import colorLogo from "../images/gether-logo.png";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const NavBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const LogoContainer = styled.div`
+  width: 170px;
+  margin: 15px;
+`;
+
+const Logo = styled.img`
+  width: 100%;
+`;
+
+const Button = styled.button`
+  color: rgb(248, 248, 248);
+  background: linear-gradient(
+    90deg,
+    rgba(32, 30, 146, 0.966),
+    rgb(134, 65, 190)
+  );
+  font-size: 20px;
+  height: 60px;
+  padding: 10px 20px;
+  border-radius: 10px;
+  border: none;
+  margin: 10px;
+  box-shadow: 0px 0px 15px 1px rgb(92, 35, 59);
+
+  &:hover {
+    background: linear-gradient(260deg, rgb(32, 185, 57), rgb(32, 185, 57));
+    cursor: pointer;
+  }
+
+  &:disabled {
+    background-color: rgb(181, 180, 180);
+    color: rgb(189, 42, 90);
+  }
+`;
 
 const Nav = ({ authToken, minimal, setShowModal, showModal, setIsSignUp }) => {
   const handleClick = () => {
     setShowModal(true);
     setIsSignUp(false);
   };
-
   return (
-    <nav>
-      <div className="logo-container">
-        <img
-          className="logo"
-          src={minimal ? colorLogo : whiteLogo}
-          alt="logo"
-        />
-      </div>
-      {!authToken && !minimal && (
-        <button
-          className="nav-button"
-          onClick={handleClick}
-          disabled={showModal}
-        >
-          Log in
-        </button>
-      )}
-    </nav>
+    <Container>
+      <NavBar>
+        <LogoContainer>
+          <Logo src={minimal ? colorLogo : whiteLogo} alt="logo" />
+        </LogoContainer>
+        {!authToken && !minimal && (
+          <Button
+            className="nav-button"
+            onClick={handleClick}
+            disabled={showModal}
+          >
+            Log in
+          </Button>
+        )}
+      </NavBar>
+    </Container>
   );
 };
+
 export default Nav;
